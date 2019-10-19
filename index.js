faa1076697118484af2855cfe498140463c91181
Newsha Tavakoli <newsha400@gmail.com>
2019-10-20 01:46:35 +0330

    remove environment variable

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-const host = process.env.HOSTNAME
-const str = 'Hello world from '
-const response = str.concat(host)
+// const host = process.env.HOSTNAME
+// const str = 'Hello world from '
+// const response = str.concat(host)
 
 console.log(response)
-app.get('/', (req, res) => res.send(response))
+app.get('/', (req, res) => res.send("Hello world!"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

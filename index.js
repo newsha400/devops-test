66eecb78fb5bf6678793d1a5a3109437625850fd
Newsha400 <newsha400@gmail.com>
2019-10-19 18:48:32 +0000

    added environment variable read

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World, from app-1!'))
+const host = process.env.HOSTNAME
+const str = 'Hello world from '
+const response = str.concat(host)
+
+console.log(response)
+app.get('/', (req, res) => res.send(response))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))

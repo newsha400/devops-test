44ad87095ae051ac5b7a1b9e95c512cd7502233e
Newsha Tavakoli <newsha400@gmail.com>
2019-10-20 02:02:12 +0330

    forgot to comment response

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3000
 // const host = process.env.HOSTNAME
 // const str = 'Hello world from '
 // const response = str.concat(host)
+//console.log(response)
 
-console.log(response)
-app.get('/', (req, res) => res.send("Hello world!"))
+app.get('/', (req, res) => res.send('Hello world!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
